e50c10a705edf707e6b9204abc3ce132383ad5cc
faiyazsadi <sadi1807073@stud.kuet.ac.bd>
2022-11-17 16:05:31 +0600

    post firebase errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:service_now/splash_screen/splash_screen.dart';
 
 void main() async {
+  await Firebase.initializeApp();
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MyApp(
